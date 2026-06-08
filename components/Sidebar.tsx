d82c07bb4d6dfb29228b112fec6c2b4f3d2f67cf
Alex <alexfacey101@gmail.com>
2026-06-08 17:01:39 -0400

    add sidebar, header, library, and site page scaffold

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,9 +2,12 @@
 
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
-import { BiSearch } from "react-icons/bi";
+import { BiLibrary, BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import Box from "./Box";
+import { CgSidebarRight } from "react-icons/cg";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 
 interface SidebarProps {
@@ -44,9 +47,29 @@ const Sidebar: React.FC<SidebarProps> = ({
             p-2
             ">
                 <Box>
-                    Sidebar Navigation
+                    <div className="
+                    flex
+                    flex-col
+                    gap-y-4
+                    px-5
+                    py-4
+                    ">
+                        {routes.map((item) => (
+                            <SidebarItem
+                            key={item.label}
+                            {...item}
+                            />
+                        ))}
+                    </div>
+                    
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    <Library/>
                 </Box>
             </div>
+            <main className="flex-1 h-full overflow-y-auto py-2">
+                {children}
+            </main>
         </div>    
     );
 };
