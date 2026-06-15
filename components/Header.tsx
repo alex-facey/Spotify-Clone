094b1b0be307134a6e0dd9a3f240b9eb547750c7
Alex <alexfacey101@gmail.com>
2026-06-14 21:53:42 -0400

    add header with auth buttons, button component, and list item

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -94,7 +95,42 @@ const Header: React.FC<HeaderProps> = ({
                     <BiSearch className="text-black" size={20} />
                 </button>
                </div>
+               <div
+                className="
+                flex
+                justify-between
+                items-center
+                gap-x-4
+                "
+            >
+                <>
+                <div>
+                    <Button
+                    onClick={() => {}}
+                    className="
+                    bg-transparent
+                    text-neutral-300
+                    font-medium
+                    ">
+                        Sign Up
+                    </Button>
+                </div>
+
+                <div>
+                    <Button
+                    onClick={() => {}}
+                    className="
+                    bg-white
+                    px-6
+                    py-2
+                    ">
+                        Log In
+                    </Button>
+                </div>
+                </>
+               </div>
            </div>
+           {children}
         </div>
     );
 }
